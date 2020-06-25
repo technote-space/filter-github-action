340977ce74482f2cc558653963c540a21dcdfb61
Technote <technote.space@gmail.com>
2020-06-26 01:17:55 +0900

    chore: code formattings

--- __tests__/context.test.ts
+++ __tests__/context.test.ts
@@ -1,74 +1,74 @@
-import { getContext } from '@technote-space/github-action-test-helper';
-import { getLabels } from '../src/context';
+import {getContext} from '@technote-space/github-action-test-helper';
+import {getLabels} from '../src/context';
 
 describe('getLabels', () => {
-	it('should return issue labels', () => {
-		expect(getLabels(getContext({
-			eventName: 'issues',
-			payload: {
-				issue: {
-					labels: [
-						{
-							name: 'test1',
-						},
-						{
-							name: 'test2',
-						},
-					],
-				},
-			},
-		}))).toEqual(['test1', 'test2']);
-	});
+  it('should return issue labels', () => {
+    expect(getLabels(getContext({
+      eventName: 'issues',
+      payload: {
+        issue: {
+          labels: [
+            {
+              name: 'test1',
+            },
+            {
+              name: 'test2',
+            },
+          ],
+        },
+      },
+    }))).toEqual(['test1', 'test2']);
+  });
 
-	it('should return PR labels', () => {
-		expect(getLabels(getContext({
-			eventName: 'pull_request',
-			payload: {
-				'pull_request': {
-					labels: [
-						{
-							name: 'test1',
-						},
-						{
-							name: 'test2',
-						},
-					],
-				},
-			},
-		}))).toEqual(['test1', 'test2']);
-	});
+  it('should return PR labels', () => {
+    expect(getLabels(getContext({
+      eventName: 'pull_request',
+      payload: {
+        'pull_request': {
+          labels: [
+            {
+              name: 'test1',
+            },
+            {
+              name: 'test2',
+            },
+          ],
+        },
+      },
+    }))).toEqual(['test1', 'test2']);
+  });
 
-	it('should return false if issue is invalid', () => {
-		expect(getLabels(getContext({
-			eventName: 'issues',
-			payload: {},
-		}))).toBe(false);
+  it('should return false if issue is invalid', () => {
+    expect(getLabels(getContext({
+      eventName: 'issues',
+      payload: {},
+    }))).toBe(false);
 
-		expect(getLabels(getContext({
-			eventName: 'issues',
-			payload: {
-				issue: {},
-			},
-		}))).toBe(false);
-	});
+    expect(getLabels(getContext({
+      eventName: 'issues',
+      payload: {
+        issue: {},
+      },
+    }))).toBe(false);
+  });
 
-	it('should return false if PR is invalid', () => {
-		expect(getLabels(getContext({
-			eventName: 'pull_request',
-			payload: {},
-		}))).toBe(false);
+  it('should return false if PR is invalid', () => {
+    expect(getLabels(getContext({
+      eventName: 'pull_request',
+      payload: {},
+    }))).toBe(false);
 
-		expect(getLabels(getContext({
-			eventName: 'pull_request',
-			payload: {
-				'pull_request': {},
-			},
-		}))).toBe(false);
-	});
+    expect(getLabels(getContext({
+      eventName: 'pull_request',
+      payload: {
+        'pull_request': {},
+      },
+    }))).toBe(false);
+  });
 
-	it('should return false if not valid event', () => {
-		expect(getLabels(getContext({
-			eventName: 'push',
-		}))).toEqual([]);
-	});
+  it('should return false if not valid event', () => {
+    expect(getLabels(getContext({
+      eventName: 'push',
+    }))).toEqual([]);
+  });
 });
